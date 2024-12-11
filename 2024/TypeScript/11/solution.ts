155bea236ce474279bef85ae333822edc418910c
Remo Rothacher <rro@nexplore.ch>
2024-12-11 21:35:32 +0100

    add ts solution for day 11

---
+++ 2024/TypeScript/11/solution.ts
@@ -0,0 +1,59 @@
+import { readInputFile } from "../../../lib";
+
+const cache = {};
+export const blink = (stone: number, blinks: number): number => {
+    let result = 0;
+    const cacheKey = `${stone}.${blinks}`;
+
+    if (cacheKey in cache) {
+        return cache[cacheKey];
+    }
+
+    const stoneString = stone.toString();
+    const amountOfDigits = stoneString.length;
+    if (blinks === 0) {
+        result = 1;
+    } else if (stone === 0) {
+        result = blink(1, blinks - 1);
+    } else if (amountOfDigits % 2 === 0) {
+        const half = amountOfDigits / 2;
+        const firstStone = Number(stoneString.slice(0, half));
+        const secondStone = Number(stoneString.slice(half));
+        result = blink(firstStone, blinks - 1) + blink(secondStone, blinks - 1);
+    } else {
+        result = blink(stone * 2024, blinks - 1);
+    }
+
+    cache[cacheKey] = result;
+
+    return result;
+};
+
+export const solution = () => {
+    console.time();
+
+    const data = readInputFile(__dirname).split(" ").map(Number);
+
+    const amountOfStones1 = data
+        .map((stone) => blink(stone, 25))
+        .reduce((acc, curr) => acc + curr, 0);
+
+    console.log("Amount of stones at 25 blinks", {
+        amountOfStones1,
+    });
+
+    console.timeEnd();
+
+    console.time();
+
+    const amountOfStones2 = data
+        .map((stone) => blink(stone, 75))
+        .reduce((acc, curr) => acc + curr, 0);
+
+    console.log("Amount of stones at 75 blinks", {
+        amountOfStones2,
+    });
+    console.timeEnd();
+};
+
+solution();
