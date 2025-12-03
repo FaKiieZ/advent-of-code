4df2a14146834a4e778138c3e8f54e9e9d043b69
Remo Rothacher <rro@nexplore.ch>
2025-12-03 19:17:57 +0100

    add ts solution for day 3

---
+++ 2025/TypeScript/03/solution.ts
@@ -0,0 +1,87 @@
+import { readInputFile } from "../../../lib";
+
+export const findHighestNumberInValidRange = (
+    bank: number[],
+    afterIndex: number,
+    amountOfBatteriesToUse: number
+): { number: number; index: number } => {
+    let sortedNumbers = new Set(
+        bank.slice(afterIndex + 1).sort((a, b) => b - a)
+    );
+
+    for (let n of sortedNumbers) {
+        let firstIndex = bank.findIndex(
+            (b, i) =>
+                b === n &&
+                i > afterIndex &&
+                i <= bank.length - amountOfBatteriesToUse
+        );
+
+        if (firstIndex > -1) {
+            return {
+                number: n,
+                index: firstIndex,
+            };
+        }
+    }
+};
+
+export const findHighestNumberAfterIndex = (
+    bank: number[],
+    startIndex: number
+): number => {
+    const possibleBankRange = bank.slice(startIndex + 1);
+    return possibleBankRange.sort((a, b) => b - a)[0];
+};
+
+export const solution = () => {
+    const batteryBanks = readInputFile(__dirname)
+        .split("\r\n")
+        .map((bank) => bank.split("").map(Number));
+
+    const activatedBatteriesPerBank = batteryBanks.map((bank) => {
+        const firstMatch = findHighestNumberInValidRange(bank, -1, 2);
+        const secondNumber = findHighestNumberAfterIndex(
+            bank,
+            firstMatch.index
+        );
+
+        return Number(`${firstMatch.number}${secondNumber}`);
+    });
+
+    const sumOfActivatedBatteries = activatedBatteriesPerBank.reduce(
+        (v, c) => v + c
+    );
+    console.log(
+        "Total output joltage with 2 batteries per pack:",
+        sumOfActivatedBatteries
+    );
+
+    const activatedBatteriesPerBank2 = batteryBanks.map((bank) => {
+        let searchAfterIndex = -1;
+        let activatedBatteries = [];
+
+        for (let i = 12; i > 0; i--) {
+            const match = findHighestNumberInValidRange(
+                bank,
+                searchAfterIndex,
+                i
+            );
+
+            searchAfterIndex = match.index;
+            activatedBatteries.push(match.number);
+        }
+
+        return Number(activatedBatteries.reduce((v, c) => `${v}${c}`));
+    });
+
+    const sumOfActivatedBatteries2 = activatedBatteriesPerBank2.reduce(
+        (v, c) => v + c
+    );
+    console.log(
+        "Total output joltage with 12 batteries per pack:",
+        sumOfActivatedBatteries2
+    );
+};
+
+solution();
